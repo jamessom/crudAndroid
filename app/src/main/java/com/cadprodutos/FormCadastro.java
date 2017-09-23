29053e0e55b375c633ae79d191ded95206ca0d33
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2017-09-23 15:21:24 -0300

    add: criação do projeto

---
+++ app/src/main/java/com/cadprodutos/FormCadastro.java
@@ -0,0 +1,13 @@
+package com.cadprodutos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+    }
+}
---
+++ app/src/main/java/com/cadprodutos/model/Produtos.java
@@ -0,0 +1,45 @@
+package com.cadprodutos.model;
+
+/**
+ * Created by jamess on 23/09/17.
+ */
+
+public class Produtos {
+
+    private Long id;
+    private String nomeProduto;
+    private String descricao;
+    private int quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
